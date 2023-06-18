21c8e5c2f909c21e8a4bdfc604150013db005778
hashtagobi1 <anokwuruobi@gmail.com>
2023-06-18 01:18:20 +0100

    basic structure

---
+++ app/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Nav = (props: Props) => {
+  return (
+    <nav>
+      <div>burger menu</div>
+      <div>logo</div>
+    </nav>
+  );
+};
+
+export default Nav;
