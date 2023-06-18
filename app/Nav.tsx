7b58b40da5ac502f0682fbc539bd94d0e8a63cf4
hashtagobi1 <anokwuruobi@gmail.com>
2023-06-18 10:22:37 +0100

    added main heading

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import BurgerMenu from "./BurgerMenu";
+import { Oi } from "next/font/google";
 
 type Props = {};
 
+const oi = Oi({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 const Nav = (props: Props) => {
   return (
-    <nav className="text-center">
-      <div>Summer Is Ours</div>
+    <nav className={` `}>
+      <h1
+        className={
+          `text-mainyellow text-center text-3xl tracking-widest ${oi.className}`
+        }
+      >
+        Summer Is Ours
+      </h1>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'Oi': ['Bodoni Moda', 'serif']
+      },
+      colors: {
+        'mainyellow': '#FEC514',
+      }
     },
   },
   plugins: [],
